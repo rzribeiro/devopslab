a4eb5030618a6dd3c62c3f88d12c016940b2a935
rzribeiro <rodrigo.zanella12@gmail.com>
2022-03-22 19:37:16 -0300

    Bug Added

--- app.py
+++ app.py
@@ -8,5 +8,14 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Rodrigo"
 
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 if __name__ == '__main__':
     app.run()
